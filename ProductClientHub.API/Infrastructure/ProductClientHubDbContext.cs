30c0d7972c388cfd69a709798a5f245a3245756e
João Victor Ventura Martins <jvmartinscv@gmail.com>
2026-03-11 10:10:24 -0300

    config entity framework

---
+++ ProductClientHub.API/Infrastructure/ProductClientHubDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ProductClientHub.API.Entities;
+
+namespace ProductClientHub.API.Infrastructure;
+
+public class ProductClientHubDbContext : DbContext
+{
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=D:\\Downloads\\1737062251373-attachment.octet-stream");
+    }
+}
--- ProductClientHub.API/obj/Debug/net10.0/ProductClientHub.API.AssemblyInfo.cs
+++ ProductClientHub.API/obj/Debug/net10.0/ProductClientHub.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductClientHub.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f81d210afaf469440282c9cb7117b5903e310e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62a7e3d441090990927f4106dfa168a2225602c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductClientHub.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductClientHub.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
